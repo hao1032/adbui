15769aadb80133fdb91de939c35c303c7903558e
tangonian <tangonian@tencent.com>
2020-04-03 09:23:53 +0800

    fix bug

--- adbui/__init__.py
+++ adbui/__init__.py
@@ -1,16 +1,12 @@
 # coding=utf-8
 from adbui.get_ui import GetUI
-from adbui.util import Util as BaseUtil
+from adbui.util import Util
 from adbui.adb_ext import AdbExt
 from adbui.tango import Tango
 
 
 class Device(GetUI):
     def __init__(self, sn=None):
-        self.util = BaseUtil(sn)
+        self.util = Util(sn)
         self.adb_ext = AdbExt(self.util)
         GetUI.__init__(self, self.adb_ext)
-
-
-class Util(BaseUtil):
-    pass
---
+++ adbui/tango.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/29 3:55 下午
+# @Author  : tangonian
+# @Site    : 
+# @File    : tango.py
+# @Software: PyCharm
+import os
+import logging
+import time
+
+
+class Tango:
+    @staticmethod
+    def set_log(log_path, level=logging.INFO, remove_exist=False):
+        if remove_exist and os.path.isfile(log_path):
+            os.remove(log_path)
+
+        handler1 = logging.StreamHandler()
+        handler2 = logging.FileHandler(filename=log_path, encoding='utf-8')
+
+        format_str = '{}%(asctime)s-%(levelname)s-%(lineno)d:%(message)s{}'
+        handler1.setFormatter(logging.Formatter(format_str.format('', '')))
+        handler2.setFormatter(logging.Formatter(format_str.format('', '')))
+
+        logging.root.level = level
+        logging.root.handlers = [handler1, handler2]
+
+    @staticmethod
+    def get_time_str():
+        return time.strftime('%Y%m%d-%H%M%S', time.localtime())
+
+    @staticmethod
+    def list_to_chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
